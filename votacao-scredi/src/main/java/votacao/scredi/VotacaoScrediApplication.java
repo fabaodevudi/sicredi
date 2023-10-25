86198f016efe55ac0e67c6699bcb5b47be571209
Fábio Pereira de Vasconcelos <fabaodevudi@gmail.com>
2023-10-24 23:46:27 -0300

    Primeiro commit

---
+++ votacao-scredi/src/main/java/votacao/scredi/VotacaoScrediApplication.java
@@ -0,0 +1,13 @@
+package votacao.scredi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacaoScrediApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotacaoScrediApplication.class, args);
+	}
+
+}
